127f5abfaed8bee71f5db2791120ea261a9a7541
SkyYap <32660903+SkyYap@users.noreply.github.com>
2024-11-29 04:08:44 +0800

    Update OpenCreditAccount.t.sol

--- contract/test/OpenCreditAccount.t.sol
+++ contract/test/OpenCreditAccount.t.sol
@@ -5,21 +5,30 @@ import "forge-std/Test.sol";
 import "../src/CreditManager.sol";
 import "../src/CreditAccount.sol";
 import "../src/USDT.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../src/PriceOracle.sol";
 import "../src/LiquidityPool.sol";
+
+contract MockUSDe is USDT {
+    constructor() USDT() {
+        _mint(msg.sender, 1_000_000 * 10**18); // Mint 1M USDe
+    }
+}
 
 contract OpenCreditAccountTest is Test {
     CreditManager public creditManager;
     PriceOracle public priceOracle;
-    USDT public collateralToken;
+    MockUSDe public collateralToken;  // Our mock USDe
     USDT public debtToken;
     LiquidityPool public liquidityPool;
     address public user;
 
     function setUp() public {
-        // Deploy all required contracts
-        collateralToken = new USDT();  // USDT as collateral
-        debtToken = new USDT();        // USDT as debt token
+        // Deploy mock USDe as collateral
+        collateralToken = new MockUSDe();
+        
+        // Deploy other contracts
+        debtToken = new USDT();
         priceOracle = new PriceOracle();
         creditManager = new CreditManager(1200, address(priceOracle));  // 120% health ratio
         liquidityPool = new LiquidityPool(
@@ -33,13 +42,16 @@ contract OpenCreditAccountTest is Test {
         user = makeAddr("user");
         vm.deal(user, 100 ether);
 
-        // Set price feed in oracle (1 USDT = $1)
+        // Set price feed in oracle for mock USDe
         vm.startPrank(address(this));
         priceOracle.setPriceFeed(
             address(collateralToken),
             0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a
         );
         vm.stopPrank();
+
+        // Transfer some mock USDe to user
+        collateralToken.transfer(user, 1000 * 10**18);
     }
 
     function testOpenCreditAccount() public {
@@ -134,5 +146,25 @@ contract OpenCreditAccountTest is Test {
         assertTrue(account1 != address(0), "User 1 account not created");
         assertTrue(account2 != address(0), "User 2 account not created");
         assertTrue(account1 != account2, "Accounts should be different");
+    }
+
+    function testCreditAccountsList() public {
+        vm.startPrank(user);
+
+        // Open credit account
+        creditManager.openCreditAccount(
+            address(collateralToken),
+            address(debtToken),
+            address(liquidityPool),
+            address(priceOracle)
+        );
+
+        // Get the created credit account address
+        address creditAccountAddr = creditManager.creditAccounts(user);
+        
+        // Check creditAccountsList - access first element with index 0
+        assertEq(creditManager.creditAccountsList(0), creditAccountAddr, "Account not added to list");
+
+        vm.stopPrank();
     }
 } 
